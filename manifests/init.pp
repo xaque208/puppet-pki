c71d6dc1a053ea5846e04ca27daa83030d94f9c6
Zach Leslie <zach@puppetlabs.com>
2013-10-16 11:51:44 -0700

    ensure backwards compat with openssl

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,4 +15,15 @@ define pki (
     require => File[$dest],
   }
 
+  file { "${dest}/openssl-0.9.8.cnf":
+    ensure  => link,
+    target  => "${dest}/openssl.cnf",
+    require => File[$dest],
+  }
+
+  file { "${dest}/openssl-0.9.6.cnf":
+    ensure  => link,
+    target  => "${dest}/openssl.cnf",
+    require => File[$dest],
+  }
 }
