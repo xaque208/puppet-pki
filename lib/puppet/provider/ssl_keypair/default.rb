a9352b9e4f2f216df57be1788e59d13643ceb587
Zach Leslie <zach@puppetlabs.com>
2013-11-02 16:33:55 -0700

    begin signing process

--- lib/puppet/provider/ssl_keypair/default.rb
+++ lib/puppet/provider/ssl_keypair/default.rb
@@ -46,6 +46,46 @@ Puppet::Type.type(:ssl_keypair).provide(:openssl) do
   end
 
   def sign_csr
+    if ca_exists?
+      debug "Signing CSR"
+
+      cmd = [
+        'ca',
+        '-batch',
+        '-out',
+        "#{@resource[:directory]}/#{@resource[:name]}.csr",
+        '-in',
+        "#{@resource[:directory]}/#{@resource[:name]}.csr",
+        '-keyfile',
+        cakeypath(),
+        '-cert',
+        cacertpath(),
+        '-outdir',
+        @resource[:directory] + '/certs',
+      ]
+      openssl(cmd)
+    else
+      fail("ca.{crt,key} were not found at the path #{@resouce[:ca_dir]}")
+    end
+  end
+
+  def ca_exists?
+    File.exists?(cacertpath()) and File.exists?(cakeypath())
+  end
+
+  def cacertpath
+    @resource[:ca_dir] + '/ca.crt'
+  end
+
+  def cakeypath
+    @resource[:ca_dir] + '/ca.key'
+  end
+
+  def certpath
+
+  end
+
+  def keypath
 
   end
 
