6665c013cda6e6caa6af5f7de449a898ec97dc30
Zach Leslie <zach@puppetlabs.com>
2013-11-02 12:40:27 -0700

    generate the csr

--- lib/puppet/provider/ssl_keypair/default.rb
+++ lib/puppet/provider/ssl_keypair/default.rb
@@ -2,8 +2,11 @@ require 'pp'
 
 Puppet::Type.type(:ssl_keypair).provide(:openssl) do
 
+  commands :openssl => 'openssl'
+
   def create
     debug "create #{resource[:name]}"
+    gen_csr()
   end
 
   def destroy
@@ -15,6 +18,28 @@ Puppet::Type.type(:ssl_keypair).provide(:openssl) do
     certpath = prefix + '.crt'
     keypath = prefix + '.key'
     File.exists?(certpath) and File.exists?(keypath)
+  end
+
+  private
+
+  def gen_csr
+    debug "Generating CSR"
+
+    cmd = [
+      'req',
+      '-batch',
+      '-days',
+      @resource[:expire],
+      '-nodes',
+      '-new',
+      '-newkey',
+      "rsa:#{@resource[:size]}",
+      '-keyout',
+      "#{@resource[:directory]}/#{@resource[:name]}.key",
+      '-out',
+      "#{@resource[:directory]}/#{@resource[:name]}.csr",
+    ]
+    openssl(cmd)
   end
 
 end
